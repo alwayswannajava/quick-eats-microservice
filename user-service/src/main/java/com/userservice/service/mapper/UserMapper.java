70ca40d8d11daff5fd8e32cc38dce16756b3aa3d
JaverJaver <mopasaj205@arpizol.com>
2025-06-05 15:20:40 +0300

    mapper

---
+++ user-service/src/main/java/com/userservice/service/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.userservice.service.mapper;
+import com.userservice.config.MapperConfig;
+import com.userservice.domain.User;
+import com.userservice.dto.request.CreateUserRequestDto;
+import com.userservice.dto.response.CreateUserResponseDto;
+import com.userservice.dto.response.FetchUserResponseDto;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    User toUser(CreateUserRequestDto createUserRequestDto);
+
+    CreateUserResponseDto toCreateUserResponseDto(User user);
+
+    FetchUserResponseDto toFetchUserResponseDto(User user);
+}
