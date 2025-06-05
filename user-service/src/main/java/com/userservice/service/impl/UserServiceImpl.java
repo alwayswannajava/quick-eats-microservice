02577aa0763bd164513050b0e98d0747ebf06f51
JaverJaver <mopasaj205@arpizol.com>
2025-06-05 14:03:39 +0300

    some changes

--- user-service/src/main/java/com/userservice/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/userservice/service/impl/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package com.userservice.service.impl;
 
 import com.userservice.dto.request.CreateUserRequestDto;
-import com.userservice.dto.request.GetUserRequestDto;
+import com.userservice.dto.request.FetchUserRequestDto;
 import com.userservice.dto.response.UpdateUserResponseDto;
 import com.userservice.repository.UserRepository;
 import com.userservice.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -18,21 +19,25 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void create(CreateUserRequestDto createUserRequestDto) {
         log.info("Service level. Creating a new user: {}", createUserRequestDto);
     }
 
     @Override
-    public GetUserRequestDto fetchUser(String userId) {
+    @Transactional(readOnly = true)
+    public FetchUserRequestDto fetchUser(String userId) {
         return null;
     }
 
     @Override
+    @Transactional
     public UpdateUserResponseDto update(String userId) {
         return null;
     }
 
     @Override
+    @Transactional
     public void delete(String userId) {
 
     }
