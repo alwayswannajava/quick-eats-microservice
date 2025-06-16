8592592138d1badbd346edf44d10e19b52a7c794
JaverJaver <mopasaj205@arpizol.com>
2025-06-16 17:29:42 +0300

    order changelog

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -7,6 +7,7 @@ import com.orderservice.dto.response.FetchOrderResponse;
 import com.orderservice.dto.response.UpdateOrderResponse;
 import com.orderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -29,29 +31,35 @@ public class OrderController {
     }
 
     @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> createOrder(CreateOrderRequest createOrderRequest) {
         log.info("------------------------POST REQUEST------------------------");
         log.info("Received request to create order");
+        orderService.create(orderMapper.toOrder(createOrderRequest));
         log.info("Order created successfully");
         log.info("------------------------POST REQUEST END------------------------");
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/fetch/{id}")
     public ResponseEntity<FetchOrderResponse> fetchOrder(@PathVariable String id) {
         log.info("------------------------GET REQUEST------------------------");
         log.info("Received request to fetch order with id: {}", id);
+        FetchOrderResponse response = orderMapper.toFetchOrderResponse(orderService.fetch(id));
         log.info("Fetched order successfully");
         log.info("------------------------GET REQUEST END------------------------");
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<UpdateOrderResponse> updateOrder(@PathVariable String id, UpdateOrderRequest updateOrderRequest) {
         log.info("------------------------PUT REQUEST------------------------");
         log.info("Received request to update order with id: {}", id);
+        UpdateOrderResponse response = orderMapper.toUpdateOrderResponse(
+                orderService.update(id, orderMapper.toOrder(updateOrderRequest)));
         log.info("Order updated successfully");
         log.info("------------------------PUT REQUEST END------------------------");
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/delete/{id}")
--- order-service/src/main/java/com/orderservice/controller/mapper/OrderMapper.java
+++ order-service/src/main/java/com/orderservice/controller/mapper/OrderMapper.java
@@ -1,4 +1,21 @@
 package com.orderservice.controller.mapper;
 
+import com.orderservice.config.MapperConfig;
+import com.orderservice.domain.Order;
+import com.orderservice.dto.request.CreateOrderRequest;
+import com.orderservice.dto.request.UpdateOrderRequest;
+import com.orderservice.dto.response.FetchOrderResponse;
+import com.orderservice.dto.response.UpdateOrderResponse;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapperConfig.class, componentModel = "spring")
 public interface OrderMapper {
+
+    Order toOrder(CreateOrderRequest createOrderRequest);
+
+    Order toOrder(UpdateOrderRequest updateOrderRequest);
+
+    FetchOrderResponse toFetchOrderResponse(Order order);
+
+    UpdateOrderResponse toUpdateOrderResponse(Order order);
 }
