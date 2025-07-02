e9fdf4b7fa8227e8160114db63711a87c741d981
JaverJaver <mykhailo.kornukh@gmail.com>
2025-07-02 23:27:14 +0300

    remove order-service folder

--- order-service/src/main/java/com/orderservice/service/exception/OrderNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.orderservice.service.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
