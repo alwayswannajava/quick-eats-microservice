cbee11602f7183bb0c31e538a0cce2143f6360d0
JaverJaver <mopasaj205@arpizol.com>
2025-07-04 23:09:47 +0300

    fix config server issue (not worked before)

--- order-service/src/main/java/com/orderservice/dto/response/OrderContactInfoResponse.java
+++ order-service/src/main/java/com/orderservice/dto/response/OrderContactInfoResponse.java
@@ -3,7 +3,6 @@ package com.orderservice.dto.response;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.util.Map;
 import java.util.List;
 
@@ -14,5 +13,4 @@ public class OrderContactInfoResponse {
     private String message;
     private Map<String, String> contactDetails;
     private List<String> onCallSupportNumbers;
-
 }
