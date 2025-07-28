fa5a83a50307a3e9344f19c6b1e900237533087d
JaverJaver <mykhailo.kornukh@gmail.com>
2025-07-28 16:32:50 +0300

    delete unnecessary files

--- order-service/src/main/java/com/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.orderservice.repository;
-
-import com.orderservice.domain.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order, String> {
-}
