118a8ebd53261df4705dc5e6d6e5ccb02a6c7d3a
JaverJaver <mykhailo.kornukh@gmail.com>
2025-08-01 20:12:27 +0300

    feign order-service

--- order-service/src/main/java/com/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/com/orderservice/repository/OrderRepository.java
@@ -4,4 +4,5 @@ import com.orderservice.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrderRepository extends CrudRepository<Order, String> {
+    Order findByContactPhone(String contactPhone);
 }
