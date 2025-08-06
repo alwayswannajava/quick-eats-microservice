7141a4e4f6d9d133241f02a1ef29d887a4a8d26f
alwayswannajava <104648050+alwayswannajava@users.noreply.github.com>
2025-08-06 23:36:24 +0300

    Delete restaurant-service directory

--- restaurant-service/src/main/java/com/restaurantservice/config/MongockConfig.java
+++
@@ -1,9 +0,0 @@
-package com.restaurantservice.config;
-
-import io.mongock.runner.springboot.EnableMongock;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableMongock
-public class MongockConfig {
-}
--- restaurant-service/src/main/java/com/restaurantservice/service/exception/RestaurantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.restaurantservice.service.exception;
-
-public class RestaurantNotFoundException extends RuntimeException {
-    public RestaurantNotFoundException(String message) {
-        super(message);
-    }
-}
