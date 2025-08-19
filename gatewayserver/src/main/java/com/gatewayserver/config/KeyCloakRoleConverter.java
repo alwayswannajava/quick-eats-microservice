7b798ffaba931c6317b137a233ba685b46b67b23
JaverJaver <mykhailo.kornukh@gmail.com>
2025-08-19 19:18:15 +0300

    fix convert method

--- gatewayserver/src/main/java/com/gatewayserver/config/KeyCloakRoleConverter.java
+++ gatewayserver/src/main/java/com/gatewayserver/config/KeyCloakRoleConverter.java
@@ -4,26 +4,26 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.stereotype.Component;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
-@Component
 public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt source) {
-        if (source.getClaims() == null || source.getClaims().isEmpty()) {
+        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+        if (realmAccess == null || realmAccess.isEmpty()) {
             return new ArrayList<>();
         }
-        return ((List<String>) source.getClaims().get("roles"))
+        return ((List<String>) realmAccess.get("roles"))
                 .stream()
                 .map(roleName -> "ROLE_" + roleName)
                 .map(SimpleGrantedAuthority::new)
                 .collect(toList());
+
     }
 }
